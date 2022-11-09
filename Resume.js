693a3d2446bfbc7c5bacb2151b8fd0719314e9a7
Mounika-Banda <99510474+Mounika-Banda@users.noreply.github.com>
2022-11-09 16:03:33 -0500

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -1,119 +1,195 @@
-let searchElement = document.querySelector("#filterJob");
+let indx = 0;
+let resumeList = [];
 
-var p= document.querySelector("#prev");
-var n= document.querySelector("#next");
+//fetch resumes to display
+const fetchResumes = async () => {
+    const response = await fetch('http://localhost:4000/resume');
+    const apiResponseResumes = await response.json();
 
-searchElement.addEventListener("keypress", setJobApplied);
+    /*.then((response) => response.json())
+    .then((response) => {
+        let apiResponseResumes = response;*/
 
-function setJobApplied(e) {
-    if (e.keyCode == 13) {
-        getResume()
+    //getting searchBar elements
+    const prev = document.querySelector(".previous");
+    const next = document.querySelector(".next");
+    const searchWord = document.querySelector("#searchInput");
+
+    const noResult = document.querySelector(".searchResultNegative");
+    const yesResult = document.querySelector(".searchResultPositive");
+
+    if (indx === 0) {
+        prev.style.visibility = "hidden";
     }
+
+    //load individual profile
+    const loadProfile = (profile) => {
+        console.log(profile);
+
+        //show or hide next & previous buttons
+        if (indx + 1 === resumeList.length) {
+            next.style.visibility = "hidden";
+        }
+        else {
+            next.style.visibility = "visible";
+        }
+
+        if (indx === 0) {
+            prev.style.visibility = "hidden";
+        }
+        else {
+            prev.style.visibility = "visible";
+        }
+
+        //get all the elements
+        const phone = document.querySelector(".phone");
+        const email = document.querySelector(".email");
+        const network = document.querySelector(".network");
+
+        const skills = document.querySelector(".tech");
+
+        const hobbies = document.querySelector(".hobbies");
+
+        const name = document.querySelector(".candidateName");
+        const appliedFor = document.querySelector(".appliedFor");
+
+        const company = document.querySelector(".company");
+        const position = document.querySelector(".position");
+        const start = document.querySelector(".start");
+        const end = document.querySelector(".end");
+        const summary = document.querySelector(".summary");
+
+        const projName = document.querySelector(".projName");
+        const projDesc = document.querySelector(".projDesc");
+
+        const ug = document.querySelector(".ug");
+        const pu = document.querySelector(".pu");
+        const school = document.querySelector(".school");
+
+        const internCompany = document.querySelector(".internCompany");
+        const internPosition = document.querySelector(".internPosition");
+        const internStartDate = document.querySelector(".internStartDate");
+        const internEndDate = document.querySelector(".internEndDate");
+        const internSummary = document.querySelector(".internSummary");
+
+        const achievements = document.querySelector(".achievements");
+
+        //set the display text for all the elements
+        phone.innerText = profile.basics.phone;
+        email.innerText = profile.basics.email;
+        network.innerText = profile.basics.profiles.network;
+        network.href = profile.basics.profiles.url;
+
+        let skillSet = profile.skills.keywords;
+        skills.innerText = "";
+        for (i = 0; i < skillSet.length; i++) {
+            skills.innerHTML += "<p>" + skillSet[i] + "</p>";
+        }
+
+        let hobbyList = profile.interests.hobbies;
+        hobbies.innerText = "";
+        for (i = 0; i < hobbyList.length; i++) {
+            hobbies.innerHTML += "<p>" + hobbyList[i] + "</p>";
+        }
+
+        name.innerText = profile.basics.name;
+        appliedFor.innerText = `Applied For - ${profile.basics.AppliedFor}`;
+
+        company.innerText = profile.work["Company Name"];
+        position.innerText = profile.work.Position;
+        start.innerText = profile.work["Start Date"];
+        end.innerText = profile.work["End Date"];
+        summary.innerText = profile.work.Summary;
+
+        projName.innerText = profile.projects.name;
+        projDesc.innerText = profile.projects.description;
+
+        ug.innerHTML = `${profile.education.UG.institute}, ${profile.education.UG.course}, ${profile.education.UG['Start Date']}, ${profile.education.UG['End Date']}, ${profile.education.UG.cgpa}`;
+        pu.innerText = `${profile.education["Senior Secondary"].institute}, ${profile.education["Senior Secondary"].cgpa}`;
+        school.innerText = `${profile.education["High School"].institute}, ${profile.education["High School"].cgpa}`;
+
+        internCompany.innerText = profile.Internship["Company Name"];
+        internPosition.innerText = profile.Internship.Position;
+        internStartDate.innerText = profile.Internship["Start Date"];
+        internEndDate.innerText = profile.Internship["End Date"];
+        internSummary.innerText = profile.Internship.Summary;
+
+        achievements.innerText = profile.achievements.Summary;
+
+    }
+
+    //loads resumeList[] with fetchAPI / search result response
+    const loadResumeList = (resumes) => {
+        console.log("inside loadResumeList " + resumes);
+
+        //if there is no search result, display no-result message
+        if (resumes.length == 0) {
+            next.style.visibility = "hidden";
+            prev.style.visibility = "hidden";
+
+            //hide the resume details div & show the no-result message div
+            yesResult.style.display = "none";
+            noResult.style.display = "flex";
+            failImage = '<img src="https://i.postimg.cc/8kBP1nFB/face-sad-512.webp" alt="No Result Found" id="noResultImage" />';
+            failMessage = '<label id="noResult"><b>No such result found.</b></label>';
+            noResult.innerHTML = failImage + failMessage;
+
+        }
+        else {
+            //show the resume details div & hide the no-result message div
+            noResult.style.display = "none";
+            yesResult.style.display = "inline";
+            resumeList = resumes;
+            indx = 0;
+            let profile = resumeList.find((profile) => resumeList.indexOf(profile) === indx);
+            loadProfile(profile);
+        }
+    }
+    //pass the fetchAPI response to loadResumeList
+    loadResumeList(apiResponseResumes);
+
+    //adding eventListener to the search input
+    searchWord.addEventListener("keypress", function (e) {
+        if (e.keyCode == '13') {
+            let searchResult = apiResponseResumes.filter(element =>
+                element.basics.AppliedFor.toLowerCase().includes(searchWord.value.toLowerCase()));
+
+            //pass the search result to loadResumeList
+            loadResumeList(searchResult);
+        }
+    })
+
+    //add click event to the previous button
+    prev.addEventListener("click", function (e) {
+
+        if (indx > 0) {
+            indx--;
+            console.log("index - " + indx);
+            profile = resumeList.find((profile) => resumeList.indexOf(profile) === indx);
+            console.log("Profile id -  " + profile.id);
+
+            //pass the profile to be loaded to loadProfile()
+            loadProfile(profile);
+        }
+    })
+
+    //add click event to the next button
+    next.addEventListener("click", function (e) {
+        if (indx < resumeList.length) {
+            indx++;
+            console.log("index - " + indx);
+            profile = resumeList.find((profile) => resumeList.indexOf(profile) === indx);
+            console.log("Profile id -  " + profile.id);
+
+            //pass the profile to be loaded to loadProfile()
+            loadProfile(profile);
+        }
+    })
+
 }
 
-function getResume(){
+fetchResumes();
 
-fetch("Data.json")
-        .then(response => response.json())
-        .then(data => {
-            for (var i = 0; i<data.resume.length; i++){
-                if (i==6){
-                    p.style.display = "block";
-                }
-                if(i==1){
-                    p.style.display = "none";
-                }
 
-                let jobApplied = data.resume[i].basics.AppliedFor;
-                
-                
-                if(searchElement.value==jobApplied){
 
-                    let id = data.resume[i].id;
-                    let name = data.resume[i].basics.name;
-                    let phone = data.resume[i].basics.phone;
-                    let email = data.resume[i].basics.email;
-                    let address = data.resume[i].basics.location.address;
-                    let postalCode = data.resume[i].basics.location.postalCode;
-                    let city = data.resume[i].basics.location.city;
-                    let state = data.resume[i].basics.location.state;
-                    let network = data.resume[i].basics.profiles.network;
-                    let url = data.resume[i].basics.profiles.url;
-                    let skillsname = data.resume[i].skills.name;
-                    let level = data.resume[i].skills.level;
-                    let keywords = data.resume[i].skills.keywords;
-                    let companyName = data.resume[i].work["Company Name"];
-                    let position = data.resume[i].work.position;
-                    let startDate = data.resume[i].work['Start Date'];
-                    let endDate = data.resume[i].work['End Date'];
-                    let summary = data.resume[i].work.Summary;
-                    let icompanyName = data.resume[i].Internship["Company Name"];
-                    let iposition = data.resume[i].Internship.Position;
-                    let istartDate = data.resume[i].Internship["Start Date"];
-                    let iEndDate = data.resume[i].Internship["End Date"];
-                    let isummary = data.resume[i].Internship.Summary;
-                    let pname = data.resume[i].projects.name;
-                    let description = data.resume[i].projects.description;
-                    let uInstitute = data.resume[i].education.UG.institute;
-                    let ucourse = data.resume[i].education.UG.course;
-                    let ustartDate = data.resume[i].education.UG["Start Date"];
-                    let uendDate = data.resume[i].education.UG["End Date"];
-                    let ucgpa = data.resume[i].education.UG.cgpa;
-                    let sinstitute = data.resume[i].education["Senior Secondary"].institute;
-                    let scgpa = data.resume[i].education["Senior Secondary"].cgpa;
-                    let asummary = data.resume[i].achievements.Summary;
-                    let hobbies = data.resume[i].interests.hobbies;
-                    document.querySelector("#fname").innerHTML += `${name}`;
-                    document.querySelector("#designation").innerHTML += `${jobApplied}`;
-                    document.querySelector("#tb1").innerHTML += `
-                    <tr>
-                        <td>${id}</td>
-                        <td>${name}</td>
-                        <td>${phone}</td>
-                        <td>${email}</td>
-                        <td>${address}</td>
-                        <td>${postalCode}</td>
-                        <td>${city}</td>
-                        <td>${state}</td>
-                        <td>${network}</td>
-                        <td>${url}</td>
-                        <td>${skillsname}</td>
-                        <td>${level}</td>
-                        <td>${keywords}</td>
-                        <td>${companyName}</td>
-                        <td>${position}</td>
-                        <td>${startDate}</td>
-                        <td>${endDate}</td>
-                        <td>${summary}</td>
-                        <td>${icompanyName}</td>
-                        <td>${iposition}</td>
-                        <td>${istartDate}</td>
-                        <td>${iEndDate}</td>
-                        <td>${isummary}</td>
-                        <td>${pname}</td>
-                        <td>${description}</td>
-                        <td>${uInstitute}</td>
-                        <td>${ucourse}</td>
-                        <td>${ustartDate}</td>
-                        <td>${uendDate}</td>
-                        <td>${ucgpa}</td>
-                        <td>${sinstitute}</td>
-                        <td>${scgpa}</td>
-                        <td>${asummary}</td>
-                        <td>${hobbies}</td>
 
-                    </tr>`;
-                   
-                    
-                    
-
-                }
-                else{
-                    document.querySelector("#noRecord").innerHTML= "Invalid Search!"
-                }
-           
-            }
-
-        })
-        
-        }
