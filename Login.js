150d7de80e1f844b71247f868b1faba939a9d55c
Mounika-Banda <99510474+Mounika-Banda@users.noreply.github.com>
2022-11-09 16:09:17 -0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "mounika" && password === "Assignment") {
         alert("You have successfully logged in.");
-        location.href = "http://127.0.0.1:5500/Resume.html";
+        location.href = "http://127.0.0.1:5500/resume.html";
         
     } else {
         loginErrorMsg.style.opacity = 1;
